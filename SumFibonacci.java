b7b3530aad551b13dc2745338bc8ed1543b4d7aa
muthukumaran001 <65880356+muthukumaran001@users.noreply.github.com>
2020-07-17 14:58:44 +0530

    Add files via upload

---
+++ SumFibonacci.java
@@ -0,0 +1,52 @@
+package mock;
+
+public class SumFibonacci {
+	
+	public static void main(String[] args) {
+		
+	
+	String str = "Java and Selenium";
+	int sum = 0;
+	
+	
+	String str2 = str.replace(" ", "");
+	//char[] ch = str.toCharArray();
+	
+	//System.out.println(str2);
+	
+	for (int i = 0; i < str2.length(); i++) {
+		
+		
+		
+		char ch = str.charAt(i);
+		
+		int num = (int) ch;
+		
+		//System.out.println(num);
+		
+		for (int j = 0; j < num; j++) {
+			
+			sum = sum + i;
+			
+			
+			
+//		}
+//	}System.out.println("sum"+sum);
+//	
+//		if(sum%2==0) {
+//			System.out.println(sum+"it is a prime");
+//		
+//		}else {
+//			System.out.println(sum+"its not a prime");
+//		}
+//	}
+	
+		
+		
+		}
+	}		
+	
+	
+	}
+	}
+
